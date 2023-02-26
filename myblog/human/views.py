2920862a0c96d3108143ea3e0a7062de48dad1a3
Saveliy <kostenko.saveliy@mail.ru>
2023-02-26 21:18:31 +0700

    django 1

--- myblog/human/views.py
+++ myblog/human/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 def index(request):
-    return
+    return HttpResponse("Создана страница")
+
+def categories(request):
+    return  HttpResponse("<h1>Статья по категории</h1>")
 
 # Create your views here.
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'human.apps.HumanConfig'
+    'human.apps.HumanConfig',
 ]
 
 MIDDLEWARE = [
