ad6b0a1168127b1d25a4c9a7a03734bd8c6458e7
Saveliy <kostenko.saveliy@mail.ru>
2023-03-18 12:32:51 +0700

    Cache and another ending element

--- myblog/article/urls.py
+++ myblog/article/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('',cache_page(60*2)(show_article.as_view()),name='article'),
-    path('create/',cache_page(60*2)(create_article.as_view()),name='create'),
+    path('',show_article.as_view(),name='article'),
+    path('create/',create_article.as_view(),name='create'),
     path('<int:pk>', articalDetail.as_view(), name='artID'),
 ]
